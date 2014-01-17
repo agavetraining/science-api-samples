eeda23b5ca2f1059750de2f799640d1a762d1fd7
Rion Dooley <dooley@tacc.utexas.edu>
2014-01-17 05:26:56 -0600

    Updating app descriptions to line their naming up with the systems from the other tutorials. Updating job submissions to line up with the revised app and system info from the other tutorials

--- apps/wc-1.00/wrapper.sh
+++ apps/wc-1.00/wrapper.sh
@@ -1,19 +1,15 @@
-
-# curl -k "http://localhost:8887/iplant-apps/trigger/job/9/token/7d7e5472e5159d726d905b4c06009c2f/status/RUNNING"
-
-IFILE=wc-1.00/read1.fq
-PLL=1
+IFILE=${query1}
+PLL=${printLongestLine}
 
 OUTPUT_FILE=wc_out.txt
 
 ARGS=" -clw"
 
 if [[ -n $PLL ]]; then
-ARGS="${ARGS}m"
+	ARGS="${ARGS}m"
 fi
 
 set -x
-# iget -f ${IFILE} .
 
 LFILE=$(basename ${IFILE})
 
@@ -22,6 +18,3 @@ wc ${ARGS} ${IFILE} > ${OUTPUT_FILE}
 set +x
 
 tar czvf output.tar.gz ${OUTPUT_FILE}
-
-
-# curl -k "http://localhost:8887/iplant-apps/trigger/job/9/token/7d7e5472e5159d726d905b4c06009c2f/status/FINISHED"
